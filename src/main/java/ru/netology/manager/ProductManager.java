9c9afa0552d8ff0a336818ba70e6b38da8b4acb6
Menshikov.SI <89089118626@mail.ru>
2023-01-12 18:03:15 +0500

    realization homework

---
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -0,0 +1,36 @@
+package ru.netology.manager;
+
+public class ProductManager {
+    private Repository repo;
+
+    public ProductManager(Repository repo) {
+        this.repo = repo;
+    }
+
+    public void addProduct(Product product) {
+        repo.addProduct(product);
+    }
+
+    public boolean matches(Product product, String search) {
+        if (product.getName().contains(search)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Product[] searchBy(String text) {
+
+        Product[] result = new Product[0];
+        for (Product product : repo.findAll()) {
+            if (matches(product, text)) {
+
+                Product[] tmp = new Product[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = product;
+                result = tmp;
+            }
+        }
+        return result;
+    }
+}
